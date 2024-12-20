a9c26b50705fb0b1315515f641a6101d79981e23
i-am-monika <mounikagarjakunta@gmail.com>
2024-12-20 10:35:40 +0530

    commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+$ cat main/java/com/example/App.java
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+~                                       
