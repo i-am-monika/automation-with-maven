14a8c8cb21e7deefeb10fa27b54fd0d39092b884
i-am-monika <mounikagarjakunta@gmail.com>
2024-12-20 10:49:35 +0530

    commit test java file

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+
